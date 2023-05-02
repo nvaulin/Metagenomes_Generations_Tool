c38f4ea63482641ec070e69ffc03aeef5aef8c06
Nikita Vaulin <vaulin@ro.ru>
2023-05-02 13:01:55 +0300

    Add insilicoseq generation

---
+++ Genomes_db_update.py
@@ -0,0 +1,103 @@
+import os
+import urllib
+import gzip
+import urllib.request
+import pandas as pd
+from Bio import Entrez, SeqIO, Seq
+from Bio.SeqRecord import SeqRecord
+
+
+def generate_ncbi_search_terms(specie):
+    terms_for_search = []
+    specie_for_search = f'"{specie}"' + '[Organism]'
+    filter_for_relevant = '(all[filter] NOT anomalous[filter])'
+    filter_for_latest = 'latest[filter]'
+    filter_for_refseq = '"latest refseq"[filter]'
+    filter_for_genbank = '"latest genbank"[filter]'
+    filters_for_taxonomy = ['" AND taxonomy check ok"[filter]', '']
+    filters_for_complete = ['" AND complete genome"[filter]', '']
+    filters_for_db = [filter_for_refseq, filter_for_genbank, filter_for_latest]
+
+    for filter_for_complete in filters_for_complete:
+        for filter_for_taxonomy in filters_for_taxonomy:
+            for filter_for_db in filters_for_db:
+                terms_temp = [specie_for_search, filter_for_relevant, filter_for_db]
+                terms_for_search.append(' AND '.join(terms_temp) + filter_for_taxonomy + filter_for_complete)
+    return specie_for_search, terms_for_search
+
+
+def update_genomes(genomes_dir, abundances):
+    if not os.path.isdir(genomes_dir):
+        os.makedirs(genomes_dir)
+    prepared_abundances = pd.DataFrame(columns=['taxid', 'abundance'])
+    assembly_summary_cols = ['RsUid', 'GbUid', 'AssemblyAccession', 'LastMajorReleaseAccession',
+                             'LatestAccession', 'ChainId', 'AssemblyName', 'UCSCName', 'EnsemblName',
+                             'Taxid', 'Organism', 'SpeciesTaxid', 'SpeciesName', 'AssemblyType',
+                             'AssemblyStatus', 'AssemblyStatusSort', 'WGS', 'GB_BioProjects',
+                             'GB_Projects', 'RS_BioProjects', 'RS_Projects', 'BioSampleAccn',
+                             'BioSampleId', 'Biosource', 'Coverage', 'PartialGenomeRepresentation',
+                             'Primary', 'AssemblyDescription', 'ReleaseLevel', 'ReleaseType',
+                             'AsmReleaseDate_GenBank', 'AsmReleaseDate_RefSeq', 'SeqReleaseDate',
+                             'AsmUpdateDate', 'SubmissionDate', 'LastUpdateDate',
+                             'SubmitterOrganization', 'RefSeq_category', 'AnomalousList',
+                             'ExclFromRefSeq', 'PropertyList', 'FromType', 'Synonym', 'ContigN50',
+                             'ScaffoldN50', 'AnnotRptUrl', 'FtpPath_GenBank', 'FtpPath_RefSeq',
+                             'FtpPath_Assembly_rpt', 'FtpPath_Stats_rpt', 'FtpPath_Regions_rpt',
+                             'Busco', 'SortOrder', 'Meta']
+
+    assembly_status_translation = {'Complete Genome': 1,
+                                   'Scaffold': 2,
+                                   'Contig': 3}
+    print(f'Checking presence of {len(abundances)} genomes')
+    for entry in range(len(abundances)):
+        specie, abundance = abundances.iloc[entry]
+        assemblies_summary = pd.DataFrame(columns=assembly_summary_cols)
+        specie_for_search, terms_for_search = generate_ncbi_search_terms(specie)
+        tax_id = Entrez.read(Entrez.esearch(db="taxonomy", term=specie))['IdList'][0]
+        fna_filename = str(tax_id) + '.fna.gz'
+        if fna_filename not in os.listdir(genomes_dir):
+            for term in terms_for_search:
+                assembly_ids = Entrez.read(Entrez.esearch(db="assembly", term=term, retmax=100))['IdList']
+                if len(assembly_ids) > 0:
+                    break
+            for assembly_id in assembly_ids:
+                assembly_summary = Entrez.read(Entrez.esummary(db="assembly", id=assembly_id, report="full"))
+                assembly_summary = pd.DataFrame(assembly_summary['DocumentSummarySet']['DocumentSummary'],
+                                                columns=assembly_summary_cols)
+                assemblies_summary = pd.concat([assemblies_summary, assembly_summary])
+
+            assemblies_summary.replace({"AssemblyStatus": assembly_status_translation}, inplace=True)
+            assemblies_sorted = assemblies_summary.sort_values(['AssemblyStatus', 'ScaffoldN50',
+                                                                'Coverage', 'LastUpdateDate', 'ContigN50'],
+                                                               ascending=[True] + [False] * 4)
+            links = assemblies_sorted[['FtpPath_RefSeq', 'FtpPath_GenBank']].head(1)
+            if links.FtpPath_RefSeq.values[0]:
+                url = links.FtpPath_RefSeq.values[0]
+            else:
+                url = links.FtpPath_GenBank.values[0]
+            label = os.path.basename(url)
+            url = url.removeprefix('ftp://')
+            url = 'https://' + url + '/' + label + '_genomic.fna.gz'
+            urllib.request.urlretrieve(url, os.path.join(genomes_dir, fna_filename))
+
+        # specie_abundance_df = pd.DataFrame([tax_id, abundance], columns=['taxid', 'abundance'])
+        # prepared_abundances = pd.concat([prepared_abundances, specie_abundance_df])
+        prepared_abundances.loc[len(prepared_abundances)] = [str(tax_id), abundance]
+    print('Genomes prepares, writing abundances.txt file')
+    prepared_abundances.abundance = prepared_abundances.abundance / prepared_abundances.abundance.sum()
+    prepared_abundances.to_csv('abundances.txt', sep='\t', index=False, header=False)
+    return prepared_abundances
+
+
+def write_multifasta(prepared_abudances, genomes_dir):
+    print('Combining the multifasta.fna')
+    sequences = []
+    for tax_id in prepared_abudances.taxid:
+        seq_str = ''
+        with gzip.open(os.path.join(genomes_dir, f'{tax_id}.fna.gz'), "rt") as handle:
+            for record in SeqIO.parse(handle, "fasta"):
+                seq_str += record.seq.strip('\n')
+        seq_record = SeqRecord(seq=seq_str, id=tax_id, name=tax_id, description='')
+        sequences.append(seq_record)
+    wr_code = SeqIO.write(sequences, os.path.join('multifasta.fna'), "fasta-2line")
+    return wr_code
--- Metagenome_generation.py
+++ Metagenome_generation.py
@@ -1,11 +1,10 @@
 import os
-import urllib
 import argparse
-import gzip
-import urllib.request
 import pandas as pd
-from Bio import Entrez, SeqIO, Seq
+from Bio import Entrez
 from pysat.examples.hitman import Hitman
+
+from Genomes_db_update import update_genomes, write_multifasta
 
 
 def parse_args():
@@ -28,102 +27,6 @@ def parse_args():
                         help='NCBI API key for Entrez requests (if any)')
 
     return parser.parse_args()
-
-
-def generate_ncbi_search_terms(specie):
-    terms_for_search = []
-    specie_for_search = f'"{specie}"' + '[Organism]'
-    filter_for_relevant = '(all[filter] NOT anomalous[filter])'
-    filter_for_latest = 'latest[filter]'
-    filter_for_refseq = '"latest refseq"[filter]'
-    filter_for_genbank = '"latest genbank"[filter]'
-    filters_for_taxonomy = ['" AND taxonomy check ok"[filter]', '']
-    filters_for_complete = ['" AND complete genome"[filter]', '']
-    filters_for_db = [filter_for_refseq, filter_for_genbank, filter_for_latest]
-
-    for filter_for_complete in filters_for_complete:
-        for filter_for_taxonomy in filters_for_taxonomy:
-            for filter_for_db in filters_for_db:
-                terms_temp = [specie_for_search, filter_for_relevant, filter_for_db]
-                terms_for_search.append(' AND '.join(terms_temp) + filter_for_taxonomy + filter_for_complete)
-    return specie_for_search, terms_for_search
-
-
-def update_genomes(genomes_dir, baseline_abudances):
-    if not os.path.isdir(genomes_dir):
-        os.makedirs(genomes_dir)
-    prepared_abudances = pd.DataFrame(columns=['taxid', 'abudance'])
-    assembly_summary_cols = ['RsUid', 'GbUid', 'AssemblyAccession', 'LastMajorReleaseAccession',
-                             'LatestAccession', 'ChainId', 'AssemblyName', 'UCSCName', 'EnsemblName',
-                             'Taxid', 'Organism', 'SpeciesTaxid', 'SpeciesName', 'AssemblyType',
-                             'AssemblyStatus', 'AssemblyStatusSort', 'WGS', 'GB_BioProjects',
-                             'GB_Projects', 'RS_BioProjects', 'RS_Projects', 'BioSampleAccn',
-                             'BioSampleId', 'Biosource', 'Coverage', 'PartialGenomeRepresentation',
-                             'Primary', 'AssemblyDescription', 'ReleaseLevel', 'ReleaseType',
-                             'AsmReleaseDate_GenBank', 'AsmReleaseDate_RefSeq', 'SeqReleaseDate',
-                             'AsmUpdateDate', 'SubmissionDate', 'LastUpdateDate',
-                             'SubmitterOrganization', 'RefSeq_category', 'AnomalousList',
-                             'ExclFromRefSeq', 'PropertyList', 'FromType', 'Synonym', 'ContigN50',
-                             'ScaffoldN50', 'AnnotRptUrl', 'FtpPath_GenBank', 'FtpPath_RefSeq',
-                             'FtpPath_Assembly_rpt', 'FtpPath_Stats_rpt', 'FtpPath_Regions_rpt',
-                             'Busco', 'SortOrder', 'Meta']
-
-    assembly_status_translation = {'Complete Genome': 1,
-                                   'Scaffold': 2,
-                                   'Contig': 3}
-
-    for entry in range(len(baseline_abudances)):
-        specie, abudance = baseline_abudances.iloc[entry]
-        assemblies_summary = pd.DataFrame(columns=assembly_summary_cols)
-        specie_for_search, terms_for_search = generate_ncbi_search_terms(specie)
-        tax_id = Entrez.read(Entrez.esearch(db="taxonomy", term=specie))['IdList'][0]
-        fna_filename = str(tax_id) + '.fna.gz'
-        if fna_filename not in os.listdir(genomes_dir):
-            for term in terms_for_search:
-                assembly_ids = Entrez.read(Entrez.esearch(db="assembly", term=term, retmax=100))['IdList']
-                if len(assembly_ids) > 0:
-                    break
-            for assembly_id in assembly_ids:
-                assembly_summary = Entrez.read(Entrez.esummary(db="assembly", id=assembly_id, report="full"))
-                assembly_summary = pd.DataFrame(assembly_summary['DocumentSummarySet']['DocumentSummary'],
-                                                columns=assembly_summary_cols)
-                assemblies_summary = pd.concat([assemblies_summary, assembly_summary])
-
-            assemblies_summary.replace({"AssemblyStatus": assembly_status_translation}, inplace=True)
-            assemblies_sorted = assemblies_summary.sort_values(['AssemblyStatus', 'ScaffoldN50',
-                                                                'Coverage', 'LastUpdateDate', 'ContigN50'],
-                                                               ascending=[True] + [False] * 4)
-            links = assemblies_sorted[['FtpPath_RefSeq', 'FtpPath_GenBank']].head(1)
-            if links.FtpPath_RefSeq.values[0]:
-                url = links.FtpPath_RefSeq.values[0]
-            else:
-                url = links.FtpPath_GenBank.values[0]
-            label = os.path.basename(url)
-            url = url.removeprefix('ftp://')
-            url = 'https://' + url + '/' + label + '_genomic.fna.gz'
-            urllib.request.urlretrieve(url, os.path.join(genomes_dir, fna_filename))
-
-            specie_abudance_df = pd.DataFrame([tax_id, abudance])
-            prepared_abudances = pd.concat([prepared_abudances, specie_abudance_df])
-
-    prepared_abudances.abudance = prepared_abudances.abudance / prepared_abudances.abudance.sum()
-    return prepared_abudances
-
-
-def write_multifasta(prepared_abudances, genomes_dir):
-    sequences = []
-    for tax_id in prepared_abudances.taxid:
-        seq_record, seq_str = Seq(), ''
-        seq_record.id = tax_id
-        seq_record.name = tax_id
-        with gzip.open(os.path.join(genomes_dir, f'{tax_id}.fna.gz'), "rt") as handle:
-            for record in SeqIO.parse(handle, "fasta"):
-                seq_str += record
-        seq_record.seq = seq_str
-        sequences.append(seq_record)
-
-    wr_code = SeqIO.write(sequences, os.path.join(genomes_dir, 'multifasta.fna'), "fasta")
-    return wr_code
 
 
 def filter_pathways_db(pathways_db):
@@ -181,15 +84,18 @@ def append_species_refill(abudances, species_to_refill):
     abudances_new[1] = abudances_new[1] / abudances_new[1].sum()
     return abudances_new
 
+
 def read_pathways(pathways_input):
-    if os.path.isfile(pathways_input.input):
+    print(pathways_input)
+    if os.path.isfile(pathways_input):
         with open(pathways_input, 'r') as f:
             pathways = f.readlines()
-    elif ',' in pathways_input.input:
-        pathways = pathways_input.input.split(',')
+    elif ',' in pathways_input:
+        pathways = pathways_input.split(',')
     else:
         raise ValueError('Invalid input. Please provide a path to a file or a comma-separated string.')
     return pathways
+
 
 if __name__ == '__main__':
     pheno = parse_args().phenotype
@@ -200,21 +106,26 @@ if __name__ == '__main__':
     api_key = parse_args().api_key
 
     Entrez.email = email
-    Entrez.api_key = api_key
-
+    if api_key is not None:
+        Entrez.api_key = api_key
     genomes_dir = os.path.join('genomes')
-    baseline_abudances = pd.read_csv(os.path.join('baseline_phenotypes', pheno + '.tsv'), sep='\t', header=None)
-    baseline_abudances.rename({0: 'species', 1: 'abundance'}, axis=1, inplace=True)
+    abundances = pd.read_csv(os.path.join('baseline_phenotypes', pheno + '.tsv'), sep='\t', header=None)
+    abundances.rename({0: 'species', 1: 'abundance'}, axis=1, inplace=True)
     if n_core:
-        baseline_abudances = baseline_abudances.sort_values(by='abundance').head(n_core)
+        n_core = min(n_core, len(abundances))
+        abundances = abundances.sort_values(by='abundance').head(n_core)
     pathways_db = pd.read_csv(os.path.join('Metabolites_database',
                                            'Pathways_MetaCyc.txt'), sep='\t').dropna(inplace=True)
-    pathways_db = filter_pathways_db(pathways_db)
-    pathways_db = preprocess_pathways_db(pathways_db)
-
-    pathways_specified = read_pathways(pathways)
-    species_to_refill = find_minimal_refill(baseline_abudances[0].to_list(),
-                                            pathways_specified)
-    abudances_new = append_species_refill(baseline_abudances, species_to_refill)
-    prepared_abudances = update_genomes(genomes_dir, abudances_new)
+    # pathways_db = filter_pathways_db(pathways_db)
+    # pathways_db = preprocess_pathways_db(pathways_db)
+    if pathways is not None:
+        print('Reading required pathways...')
+        pathways_specified = read_pathways(pathways)
+        species_to_refill = find_minimal_refill(abundances[0].to_list(),
+                                                pathways_specified)
+        abundances = append_species_refill(abundances, species_to_refill)
+    prepared_abudances = update_genomes(genomes_dir, abundances)
     wr_code = write_multifasta(prepared_abudances, genomes_dir)
+    print('\n')
+    iss_cmd = 'iss generate -g multifasta.fna --abundance_file abundances.txt --cpus 8 -m miseq -o miseq_reads'
+    os.system(iss_cmd)
